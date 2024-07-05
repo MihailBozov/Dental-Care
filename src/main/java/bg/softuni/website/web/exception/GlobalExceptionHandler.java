4d59e6995d378f1bb5725a9c09127fd45d055bd3
MihailBozov <mihailbozov001@gmail.com>
2024-07-05 19:44:28 +0300

    Http error pages built

--- src/main/java/bg/softuni/website/web/exception/GlobalExceptionHandler.java
+++ src/main/java/bg/softuni/website/web/exception/GlobalExceptionHandler.java
@@ -1,17 +1,20 @@
-//package bg.softuni.website.web.exceptionHandlers;
-//
-//import bg.softuni.website.util.exception.ObjectNotFoundException;
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//@ControllerAdvice
-//public class GlobalExceptionHandler {
-//    
+package bg.softuni.website.web.exception;
+
+import bg.softuni.website.util.exception.ObjectNotFoundException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
 //    @ExceptionHandler(ObjectNotFoundException.class)
-//    public ModelAndView handleObjectNotFound(ObjectNotFoundException e) {
-//        ModelAndView modelAndView = new ModelAndView("errors-controller-based/not-found-global");
-//        modelAndView.addObject("id", e.getId());
-//        return modelAndView;
+//    public String handleObjectNotFound(ObjectNotFoundException e, Model model) {
+//
+//        model.addAttribute("id", e.getId());
+//        return "errors-controller-based/not-found-global";
 //    }
-//}
+
+}
